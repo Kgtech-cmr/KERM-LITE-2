52a08d4cc6917a8a8986332549b214733e862587
KermRayan. <kermd237@gmail.com>
2024-09-20 21:28:08 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "put season id",
 MONGODB: process.env.MONGODB || "mongodb://mongo:MfPJLADhRWNHvxuOORrlmfJHboedaADB@junction.proxy.rlwy.net:57828",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/397000a07a1deb7fad9c2.jpg",
-ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi GHOST-MD Is Online Now 💻\n*💻 Owner* - GHOST-MD\n\n*💻 Owner Number* -94741140620",
-SUDO_NB: process.env.SUDO_NB || "94741140620",
+ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi KERM-LITE-2 Is Online Now 💻\n*💻 Owner* - KERM-LITE-2\n\n*💻 Owner Number* +237659535227",
+SUDO_NB: process.env.SUDO_NB || "237650564445",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE:"true"
