5c1b7455b85b5144e9eb9c5289acaa3db536c6ca
KermRayan. <kermd237@gmail.com>
2024-09-20 21:17:26 +0100

    Update repo.js

--- plugins/repo.js
+++ plugins/repo.js
@@ -2,6 +2,7 @@ const {cmd , commands} = require('../command')
 
 cmd({
     pattern: "repo",
+    alias: ["kerm"],
     desc: "repo the bot",
     category: "main",
     react: "📡",
@@ -11,21 +12,21 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-let dec = `*👾 GHOST MD Repastitory Information*
+let dec = `*👾 KERM LITE 2 Repastitory Information*
 
-*| ɴᴀᴍᴇ*: ɢʜᴏꜱᴛ-ᴍᴅ
-*| ᴏᴡɴᴇʀ*: ᴄʏʙᴇʀ ɢʜᴏꜱᴛ ( ɢʜᴏꜱᴛ-ᴍᴅ )
-*| ɴᴜᴍʙᴇʀ*: 94741140620
+*| ɴᴀᴍᴇ*: Kᴇʀᴍ-Lɪᴛᴇ-2
+*| ᴏᴡɴᴇʀ*: Kɢ Tᴇᴄʜ ( Kᴇʀᴍ-Lɪᴛᴇ-2 )
+*| ɴᴜᴍʙᴇʀ*: +237659535227 | +237650564445
 *| ᴠᴇʀꜱɪᴏɴ*: 1.0.0
 
 
 *📡 REPO LINK*
-🔗◦https://github.com/GHOST-V1-MD/GHOST-MD.git
+🔗◦https://github.com/Kgtech-cmr/KERM-LITE-2
 
 *📌 SUBSCRIBE MY YOUTUBE CHANNEL*
-🔗◦ https://youtube.com/@cyberghost630?si=JVTDEQ8vO9Ksgv9M
+🔗◦ https://youtube.com/@kermhacktools-s9s
 
-©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɢʜᴏꜱᴛ-ᴍᴅ
+©ᴘᴏᴡᴇʀᴇᴅ ʙʏ Kᴇʀᴍ-Lɪᴛᴇ-2
 `
 await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/397000a07a1deb7fad9c2.jpg`},caption:dec},{quoted:mek});
 
