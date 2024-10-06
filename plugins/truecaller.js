b04570a30a7dc348433ef238f807c31986c83b89
KermRayan <kermd237@gmail.com>
2024-10-06 16:45:53 +0100

    Update truecaller.js

--- plugins/truecaller.js
+++ plugins/truecaller.js
@@ -1,11 +1,9 @@
 /*------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-Copyright (C) 2023 Loki - Xer.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-Jarvis - Loki-Xer 
-
+Copyright (C) 2024 Kgtech-cmr.
+Sous licence GPL-3.0 ; vous ne pouvez pas utiliser ce fichier sauf en conformité avec la licence sous peine de poursuites judiciaires.
+Kgtech-cmr.
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
@@ -23,22 +21,22 @@ System({
 }, async (message, match) => {
 	if (match.match(/login/gi)) {
 		match = match.replace(/login/gi, '');
-		if (!match) return await message.send('_give me a number to send otp_');
+		if (!match) return await message.send('_Donne-moi un numéro pour envoyer un otp_');
 		const msg = await truecaller.set(match);
-		if (msg === true) return await message.send(`_successfully send otp to this ${match} number_\n_use *true otp* <key> to login_`);
-		return await message.send(`*message:* _use *true logout* as first_\n*resone*: ${msg}`);
+		if (msg === true) return await message.send(`_Envoyer avec succès otp à ce ${match} numéro_\n_utilise *true otp* <key> pour se connecter_`);
+		return await message.send(`*Message :* _utiliser *true logout* comme premier_\n*resone*: ${msg}`);
 	} else if (match.match(/logout/gi)) {
 		await truecaller.logout(match);
-		return await message.send(`_successfull_`); 
+		return await message.send(`_Réussi_`); 
 	} else if (match.match(/otp/gi)) {
 		match = match.replace(/otp/gi, '');
-		if (!match) return await message.send('_please provide an otp_');
+		if (!match) return await message.send('_Veuillez fournir un otp_');
 		const msg = await truecaller.otp(match);
-		if (msg === true) return await message.send(`_successfully Logined to Truecaller!!_`);
-		return await message.send(`*message:* _use *true logout* as first_\n*resone*: ${msg}`);
+		if (msg === true) return await message.send(`_Connecté avec succès à Truecaller!!_`);
+		return await message.send(`*Message :* _utiliser *true logout* comme premier_\n*resone*: ${msg}`);
 	}
 	let user = (message.mention.jid?.[0] || message.reply_message.mention.jid?.[0] || message.reply_message.sender || match).replace(/[^0-9]/g, '');
-	if (!user) return await message.send(`_reply to a user_`)
+	if (!user) return await message.send(`_Répondre à un utilisateur_`)
 	const res = await truecaller.search(user);
 	if (!res.status) return await message.send(res.message);
 	let msg = `╭─❮ truecaller ❯ ❏\n`
