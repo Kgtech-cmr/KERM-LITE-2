2e645b5abb5529eebb4e7fd2ccee488ce26a3178
Fareno237 <lerfareno@gmail.com>
2024-10-06 02:48:34 +0100

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -1,10 +1,10 @@
 /*------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-Copyright (C) 2023 Loki - Xer.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-Jarvis - Loki-Xer 
+Copyright (C) 2024 Kgtech-cmr.
+Sous licence GPL-3.0 ; vous ne pouvez pas utiliser ce fichier sauf en conformité avec la licence sous peine de poursuites judiciaires.
+Kgtech-cmr.
+
 
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------*/
@@ -24,27 +24,27 @@ const {
 System({
     pattern: "thinkany", 
     fromMe: isPrivate,
-    desc: "ai thinkany", 
+    desc: "thinkany ia", 
     type: "ai",
 }, async (message, match, m) => {
     match = match || m.reply_message.text;
     if(match && m.quoted) match = match + m.reply_message.text;
-    if(!match) return m.reply("_*need query !!*_\n_*eg: .thinkany who is iron man*_");
+    if(!match) return m.reply("_*J ai besoin d une requête s'il vous plaît!!*_\n_*exemple: .thinkany qui est le coach de Manchester City*_");
     const { result } = await interactWithAi("thinkany", match);
-    await m.send(result, { contextInfo: { forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363197401188542@newsletter', newsletterName: 'ᴛʜɪɴᴋᴀɴʏ ᴀɪ' }}});
+    await m.send(result, { contextInfo: { forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363321386877609@newsletter', newsletterName: 'ᴛʜɪɴᴋᴀɴʏ ᴀɪ' }}});
 });
 
 System({
     pattern: "aoyo", 
     fromMe: isPrivate,
-    desc: "ai aoyo", 
+    desc: "aoyo ia", 
     type: "ai",
 }, async (message, match, m) => {
     match = match || m.reply_message.text;
     if(match && m.quoted) match = match + m.reply_message.text;
-    if(!match) return m.reply("_*need query !!*_\n_*eg: .aoyo who is iron man*_");
+    if(!match) return m.reply("_*J ai besoin d une requête s'il vous plaît !!*_\n_*exemple: .aoyo qui est Elon Musk*_");
     const { result } = await interactWithAi("aoyo", match);
-    await m.send(result, { contextInfo: { forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363197401188542@newsletter', newsletterName: 'ᴀᴏʏᴏ ᴀɪ' }}});
+    await m.send(result, { contextInfo: { forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363321386877609@newsletter', newsletterName: 'ᴀᴏʏᴏ ᴀɪ' }}});
 });
 
 System({
@@ -55,10 +55,10 @@ System({
 }, async (message, match, m) => {
     match = match || m.reply_message.text;
     if(match && m.quoted) match = match + m.reply_message.text;
-    if(!match) return m.reply("_*need query !!*_\n_*eg: .prodia a girl in full moon*_");
-    await m.reply("*please wait generating*");
+    if(!match) return m.reply("_*J ai besoin d une requête !!*_\n_*exemple: .prodia un chien sur une moto*_");
+    await m.reply("*S il vous plaît patientez on s'occupe de ça pour vous 😄✨*");
     const img = await interactWithAi("prodia", match);
-    await m.sendFromUrl(img, { contextInfo: { forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363197401188542@newsletter', newsletterName: 'ᴩʀᴏᴅɪᴀ ᴀɪ' }}});
+    await m.sendFromUrl(img, { contextInfo: { forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363321386877609@newsletter', newsletterName: 'ᴩʀᴏᴅɪᴀ ᴀɪ' }}});
 });
 
 
@@ -70,10 +70,10 @@ System({
 }, async (message, match, m) => {
     match = match || m.reply_message.text;
     if(match && m.quoted) match = match + m.reply_message.text;
-    if(!match) return m.reply("_*need query !!*_\n_*eg: .dalle a girl in full moon*_");
-    await m.reply("*please wait generating*");
+    if(!match) return m.reply("_*J ai besoin d une requête s'il vous plaît !!*_\n_*exemple: .dalle un chien sur une moto*_");
+    await m.reply("*S il vous plaît patientez on s'occupe de ça pour vous 😄✨*");
     const img = await interactWithAi("dalle", match);
-    await m.sendFromUrl(img, { contextInfo: { forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363197401188542@newsletter', newsletterName: 'ᴅᴀʟʟᴇ ᴀɪ' }}});
+    await m.sendFromUrl(img, { contextInfo: { forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363321386877609@newsletter', newsletterName: 'ᴅᴀʟʟᴇ ᴀɪ' }}});
 });
 
 
@@ -85,9 +85,9 @@ System({
 }, async (message, match, m) => {
     match = match || m.reply_message.text;
     if(match && m.quoted) match = match + m.reply_message.text;
-    if(!match) return m.reply("_*need query !!*_\n_*eg: .lepton who is iron man*_");
+    if(!match) return m.reply("_*J ai besoin d une requête s'il vous plaît !!*_\n_*exemple: .lepton qui est le président du Cameroun*_");
     const { result } = await interactWithAi("lepton", match);
-    await m.send(result.replace(/\[[^\]]*\]|\([^)]*\)|<[^>]*>/g, ''), { contextInfo: { forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363197401188542@newsletter', newsletterName: 'ʟᴇᴩᴛᴏɴ ᴀɪ' }}});
+    await m.send(result.replace(/\[[^\]]*\]|\([^)]*\)|<[^>]*>/g, ''), { contextInfo: { forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363321386877609@newsletter', newsletterName: 'ʟᴇᴩᴛᴏɴ ᴀɪ' }}});
 });
 
 System({
@@ -98,9 +98,9 @@ System({
 }, async (message, match, m) => {
     match = match || m.reply_message.text;
     if(match && m.quoted) match = match + m.reply_message.text;
-    if(!match) return m.reply("_*need query !!*_\n_*eg: .chatgpt who is iron man*_");
+    if(!match) return m.reply("_*J ai besoin d une requête s'il vous plaît !!*_\n_*exemple: .chatgpt qui est Paul Biya*_");
     const { response } = await interactWithAi("gpt", match);
-    await m.send(response, { contextInfo: { forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363197401188542@newsletter', newsletterName: 'ᴄʜᴀᴛɢᴩᴛ 4' }}});
+    await m.send(response, { contextInfo: { forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: 120363321386877609@newsletter', newsletterName: 'ᴄʜᴀᴛɢᴩᴛ 4' }}});
 });
 
 System({
@@ -111,9 +111,9 @@ System({
 }, async (message, match, m) => {
        match = match || m.reply_message.text;
        if(match && m.quoted) match = match + m.reply_message.text;
-       if(!match) return m.reply("_*need query !!*_\n_*eg: .bb who is iron man*_");
+       if(!match) return m.reply("_*J ai besoin d une requête s'il vous plaît !!*_\n_*exemple: .bb qui est Nelson Mandela*_");
        const { result } = await interactWithAi("blackbox", match);
-       await m.send(result, { contextInfo: { forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363197401188542@newsletter', newsletterName: 'ʙʟᴀᴄᴋ ʙᴏx' }}});
+       await m.send(result, { contextInfo: { forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363321386877609@newsletter', newsletterName: 'ʙʟᴀᴄᴋ ʙᴏx' }}});
 });
 
 System({
@@ -124,9 +124,9 @@ System({
 }, async (message, match, m) => {
     match = match || message.reply_message.text;
     if (match && m.quoted) match = message.reply_message.text + '\n' + match;   
-    if (!match) return m.reply("_*Need Query !!*_\n_*eg: .gemini who is iron man?*_");  
+    if (!match) return m.reply("_*J ai besoin d une requête s'il vous plaît !!*_\n_*exemple: .gemini pourquoi la femme est compliquée?*_");  
     const { content } = await postJson(`https://ronak-api.onrender.com/ronak/gemini`, { prompt: match });
-    await m.send(content, { contextInfo: { forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363197401188542@newsletter', newsletterName: 'ɢᴇᴍɪɴɪ ᴀɪ' }}});
+    await m.send(content, { contextInfo: { forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363321386877609@newsletter', newsletterName: 'ɢᴇᴍɪɴɪ ᴀɪ' }}});
 });
 
 System({
@@ -137,9 +137,9 @@ System({
 }, async (message, match, m) => {
     match = match || m.reply_message.text;
     if(match && m.quoted) match = match + m.reply_message.text;
-    if(!match) return m.reply("_*need query !!*_\n_*eg: .chatgpt who is iron man*_");
+    if(!match) return m.reply("_*J ai besoin d une requête s'il vous plaît !!*_\n_*exemple: .chatgpt Qui est Samuel Etoo*_");
     const response = await interactWithAi("chatgpt", match);
-    await m.send(response, { contextInfo: { forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363197401188542@newsletter', newsletterName: 'ᴄʜᴀᴛɢᴩᴛ' }}});
+    await m.send(response, { contextInfo: { forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363321386877609@newsletter', newsletterName: 'ᴄʜᴀᴛɢᴩᴛ' }}});
 });
 
 System({
@@ -148,10 +148,10 @@ System({
     desc: 'Enhance images with AI',
     type: 'ai',
 }, async (message, match) => {
-    if (!message.quoted || !message.reply_message.image) return await message.send("Reply to an image baka!");
+    if (!message.quoted || !message.reply_message.image) return await message.send("Réponds à une image idiot 🤦🏽!");
     const img = await message.reply_message.downloadAndSave();
     const upscale = await interactWithAi("upscale", img);
-    await message.send(upscale, { caption: "_*upscaled 🍉*_" }, "img");
+    await message.send(upscale, { caption: "_*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋᴇʀᴍ-ᴍᴅ*_" }, "img");
 });
 
 System({
@@ -160,12 +160,12 @@ System({
 	desc: 'Text Recognition from image',
 	type: 'ai',
 }, async (message, match) => {
-    if(!message.reply_message.image) return await message.reply("_Reply to a image_");
+    if(!message.reply_message.image) return await message.reply("_Réponds à une image werrr 🤦🏽_");
     const data = await GraphOrg(await message.reply_message.downloadAndSaveMedia());
     const res = await fetch(IronMan(`ironman/ai/ocr?url=${data}`));
-    if (res.status !== 200) return await message.reply('*Error*');
+    if (res.status !== 200) return await message.reply('*Oups! Quelque chose n a pas marché 🚫*');
     const ress = await res.json();
-    if (!ress.text) return await message.reply('*Not found*');
+    if (!ress.text) return await message.reply('*J ai pas trouvé*');
     await message.reply(`\`\`\`${ress.text}\`\`\``);
 });
 
